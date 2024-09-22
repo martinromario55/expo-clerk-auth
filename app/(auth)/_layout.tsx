996d22b2181214bdc66e8a86a96911447595eb6a
martinromario55 <martinromario55@gmail.com>
2024-09-22 22:07:07 +0300

    Complete authentication

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { Pressable } from 'react-native'
+import { useAuth } from '@clerk/clerk-expo'
 
-const AuthLayout = () => {
+export const LogoutButton = () => {
+  const { signOut } = useAuth()
+
+  const doLogout = () => {
+    signOut()
+  }
+
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <Pressable onPress={doLogout} style={{ marginRight: 10 }}>
+      <Ionicons name="log-out-outline" size={24} color={'#fff'} />
+    </Pressable>
   )
 }
 
-export default AuthLayout
+const TabsPage = () => {
+  const { isSignedIn } = useAuth()
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6c47ff',
+        },
+        headerTintColor: '#fff',
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerTitle: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+          tabBarLabel: 'Home',
+        }}
+        redirect={!isSignedIn}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: 'My Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+          tabBarLabel: 'My Profile',
+          headerRight: () => <LogoutButton />,
+        }}
+        redirect={!isSignedIn}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsPage
